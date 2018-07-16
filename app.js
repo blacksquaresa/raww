6a8823c3067952b1eaa5139cfe1652bae4ac6e93
Gareth Cornish <gareth@blacksquare.co.za>
2018-07-16 16:58:21 +0200

    Create worker as a data blob

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-class Adder{
-    add(x, y){
-        return x+y;
+const workerCode = () => {
+    function add(x, y){
+        return Number(x)+Number(y);
     }
+    
+    self.addEventListener('message', function(e) {
+        let result = add(e.data.x, e.data.y);
+        self.postMessage(result);
+      }, false);
 }
+const workerBlob = new Blob(['(', workerCode.toString(), ')();'], { type: 'text/javascript' });
 
-window.adder = new Adder();
 let num1 = document.getElementById('num1');
 let num2 = document.getElementById('num2');
 let equals = document.getElementById('equals');
 let answer = document.getElementById('answer');
-let worker = new Worker('worker.js');
+let worker = new Worker(window.URL.createObjectURL(workerBlob));
 
 worker.addEventListener('message', function(e) {
     answer.innerText = e.data;
@@ -18,3 +23,4 @@ worker.addEventListener('message', function(e) {
 equals.addEventListener('click', () => {
     worker.postMessage({ x: Number(num1.value), y: Number(num2.value)});
 }, false);
+
--- worker.js
+++
@@ -1,8 +0,0 @@
-function add(x, y){
-    return Number(x)+Number(y);
-}
-
-self.addEventListener('message', function(e) {
-    let result = add(e.data.x, e.data.y);
-    self.postMessage(result);
-  }, false);

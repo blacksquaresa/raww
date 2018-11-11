a448484c5b6ff58bb1d4e0d39b76028d45339538
gareth Cornish <gareth@blacksquare.co.za>
2018-11-11 08:27:14 +0200

    Update tests to expect es5 compliant template code

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -3,43 +3,43 @@ import { functionToString, getDependencyConstructor, objectToString } from '../s
 describe("#getDependencyConstructor", () => {
   it('returns correct constructor string for a string dependency', () => {
     const result = getDependencyConstructor({ name:'myname', dependency: 'my dependency' });
-    expect(result).toBe("const myname = 'my dependency';");
+    expect(result).toBe("var myname = 'my dependency';");
   });
   
   it('returns correct constructor string for a number dependency', () => {
     const result = getDependencyConstructor({ name:'myname', dependency: 42 });
-    expect(result).toBe("const myname = 42;");
+    expect(result).toBe("var myname = 42;");
   });
 
   it('returns correct constructor string for a boolean dependency', () => {
     const result = getDependencyConstructor({ name:'myname', dependency: true });
-    expect(result).toBe("const myname = !!1;");
+    expect(result).toBe("var myname = !!1;");
   });
   
   it('returns correct constructor string for an arrow function dependency', () => {
     const myfunc = () => { console.log('my func'); };
     const result = getDependencyConstructor({ name:'myname', dependency: myfunc });
-    expect(result).toBe("const myname = () => { console.log('my func'); };");
+    expect(result).toBe("var myname = () => { console.log('my func'); };");
   });
   
   it('returns correct constructor string for a function dependency', () => {
     const myfunc = function() { console.log('my func'); };
     const result = getDependencyConstructor({ name:'myname', dependency: myfunc });
-    expect(result).toBe("const myname = function () { console.log('my func'); };");
+    expect(result).toBe("var myname = function () { console.log('my func'); };");
   });
 });
 
 describe("#functionToString", () => {
   it('returns default for a non function', () => {
     const result = functionToString(('not a function' as any));
-    expect(result).toBe('()=>{}');
+    expect(result).toBe('function(){}');
   });
 
   it('returns default for a function with an overridden toString method', () => {
     const func = () => { var x=1; };
     func.toString = () => '';
     const result = functionToString(func);
-    expect(result).toBe('()=>{}');
+    expect(result).toBe('function(){}');
   });
 
   it('returns constructor string for a function', () => {
